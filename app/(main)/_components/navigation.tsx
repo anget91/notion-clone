e5aa05bff8f68604d245c14339a50e13240f0b8b
Angelica Toloza- <atolozafranco13@gmail.com>
2025-01-21 17:03:24 -0500

    feat: add UserItem component for user profile display and dropdown menu; update Navigation and Documents page

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import { ChevronsLeft, MenuIcon } from 'lucide-react';
 import { usePathname } from 'next/navigation';
 import { use, useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
+import UserItem from './user-item';
 
 export default function Navigation() {
   const pathname = usePathname();
@@ -107,7 +108,7 @@ export default function Navigation() {
           <ChevronsLeft className="size-6" />
         </div>
         <div>
-          <p>Action items</p>
+          <UserItem />
         </div>
         <div className="mt-4">
           <p>Documents</p>
